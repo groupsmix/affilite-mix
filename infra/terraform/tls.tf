cfdbf97f99d2f0817bf59d355baf4dbfb869af86
groupsmix <professional.inbox.simo@gmail.com>
2026-05-01 18:49:13 +0100

    chore(compliance): add infra/terraform/tls.tf

--- infra/terraform/tls.tf
+++ infra/terraform/tls.tf
@@ -1,9 +1,4 @@
-# OF-29: prefer TLS 1.3 only at edge.
 resource "cloudflare_zone_settings_override" "tls13" {
   zone_id = var.cloudflare_zone_id
-  settings {
-    min_tls_version = "1.3"
-    tls_1_3         = "on"
-    automatic_https_rewrites = "on"
-  }
+  settings { min_tls_version = "1.3" tls_1_3 = "on" automatic_https_rewrites = "on" }
 }
