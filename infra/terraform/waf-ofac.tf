9bd4691007e3eff9c41ad90def801062117d84a1
groupsmix <professional.inbox.simo@gmail.com>
2026-05-01 18:48:12 +0100

    chore(compliance): add infra/terraform/waf-ofac.tf

---
+++ infra/terraform/waf-ofac.tf
@@ -0,0 +1,13 @@
+# OF-30: hard block restricted geos for OFAC compliance.
+resource "cloudflare_ruleset" "ofac_block" {
+  zone_id = var.cloudflare_zone_id
+  name    = "ofac-hard-block"
+  kind    = "zone"
+  phase   = "http_request_firewall_custom"
+  rules {
+    action = "block"
+    expression = "(ip.geoip.country in {\"IR\" \"KP\" \"SY\" \"CU\"})"
+    description = "Hard-block OFAC restricted countries"
+    enabled = true
+  }
+}
